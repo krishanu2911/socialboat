77b076703631feb786bf8d9892fff57dfd5cd583
Krishanu Dutta <hrickkrishanudutta@gmail.com>
2023-09-16 23:06:15 +0530

    feat: debounce in search bar

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,13 +1,27 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 export const SearchBar = () => {
-  const [searchQuery, SetSearchQuery] = useState("");
-  const changeHandler = () => {
-    
-  }
+  const [searchQuery, setSearchQuery] = useState("");
+  const fetchVideoData = (query) => {
+    console.log("calling api", query);
+  };
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      fetchVideoData(searchQuery);
+    }, 500);
+
+    return () => clearTimeout(timer);
+  }, [searchQuery]);
+
   return (
     <div className="shadow-md px-6 py-2 rounded-md md:w-[60%]">
-      <input placeholder="search" className="w-full outline-[transparent]" />
+      <input
+        value={searchQuery}
+        onChange={(e) => setSearchQuery(e.target.value)}
+        placeholder="search"
+        className="w-full outline-[transparent]"
+      />
     </div>
   );
 };
